8fd75c8a52ea09830a4d6d5fd70544e4befab4d1
Fidane Tağızade <tagizadefidane@gmail.com>
2026-05-30 04:59:43 +0400

    feat:fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
-import './App.css';
+import  { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import ListPage from "./components/ListPage";
 
 function App() {
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [lists, setLists] = useState([]);
+
+ 
+  const handleAddToFavorite = (movie) => {
+    if (!favoriteMovies.some((item) => item.imdbID === movie.imdbID)) {
+      setFavoriteMovies([...favoriteMovies, movie]);
+    }
+  };
+
+ 
+  const handleRemoveFromFavorite = (imdbID) => {
+    setFavoriteMovies(favoriteMovies.filter((item) => item.imdbID !== imdbID));
+  };
+
+
+  const handleCreateList = (title) => {
+    if (favoriteMovies.length === 0) return;
+    const newList = {
+      title: title,
+      movies: [...favoriteMovies],
+    };
+    setLists([...lists, newList]);
+    setFavoriteMovies([]); 
+  };
+
+  const handleDeleteList = (indexToDelete) => {
+    setLists(lists.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
-    <div className='app'>
-      <Header />
-      <Main />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Main
+                favoriteMovies={favoriteMovies}
+                onAddToFavorite={handleAddToFavorite}
+                onRemoveFromFavorite={handleRemoveFromFavorite}
+                onCreateList={handleCreateList}
+                lists={lists}
+              />
+            }
+          />
+          <Route
+            path="/list"
+            element={<ListPage lists={lists} onDeleteList={handleDeleteList} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
