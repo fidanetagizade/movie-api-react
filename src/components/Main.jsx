86f7758eab5a3ff37c56af5e40df1077b3fc3eed
Fidane Tağızade <tagizadefidane@gmail.com>
2026-05-30 04:59:19 +0400

    feat: add Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import "./Main.css";
 import Filter from "./Filter";
 import Card from "./Card"; 
+import Favorite from "./Favorite";
 
-function Main() {
+function Main({ favoriteMovies, onAddToFavorite, onRemoveFromFavorite, onCreateList, lists }) {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("harry potter");
 
@@ -28,15 +29,34 @@ function Main() {
     <main>
       <Filter onSearch={setSearchQuery} />
 
-      <div className="movies-container">
-        {movies.length > 0 ? (
-          movies.map((item) => (
-          
-            <Card key={item.imdbID} movie={item} />
-          ))
-        ) : (
-          <p className="no-movies">Loading...</p>
-        )}
+      
+      <div className="main-layout">
+        
+        <div className="movies-container">
+          {movies.length > 0 ? (
+            movies.map((item) => {
+              const isAdded = favoriteMovies.some((fav) => fav.imdbID === item.imdbID);
+              return (
+                <Card 
+                  key={item.imdbID} 
+                  movie={item} 
+                  onAddToFavorite={onAddToFavorite}
+                  isAdded={isAdded}
+                />
+              );
+            })
+          ) : (
+            <p className="no-movies">Loading...</p>
+          )}
+        </div>
+
+        <Favorite 
+          favoriteMovies={favoriteMovies}
+          onRemoveFromFavorite={onRemoveFromFavorite}
+          onCreateList={onCreateList}
+          lists={lists}
+        />
+
       </div>
     </main>
   );
